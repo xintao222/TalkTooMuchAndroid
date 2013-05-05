017a01c266fee4c5c89fee018dea0ff759eee37b
Ian Wilson <isherifwilson@gmail.com>
2013-05-05 16:02:47 -0400

    All the fixes

--- Talk Too Much/gen/com/example/soundanalyser/R.java
+++ Talk Too Much/gen/com/example/soundanalyser/R.java
@@ -27,15 +27,22 @@ containing a value of this type.
         public static final int minValue=0x7f010000;
     }
     public static final class color {
-        public static final int darkgray_color=0x7f070002;
-        public static final int gray_color=0x7f070001;
-        public static final int greenEnd=0x7f070008;
-        public static final int greenMid=0x7f070007;
-        public static final int greenStart=0x7f070006;
-        public static final int redEnd=0x7f070005;
-        public static final int redMid=0x7f070004;
-        public static final int redStart=0x7f070003;
-        public static final int white_color=0x7f070000;
+        public static final int blue2=0x7f050003;
+        public static final int blue25=0x7f050004;
+        public static final int darkgray_color=0x7f050009;
+        public static final int gray_color=0x7f050008;
+        public static final int greenEnd=0x7f05000f;
+        public static final int greenMid=0x7f05000e;
+        public static final int greenStart=0x7f05000d;
+        public static final int grey05=0x7f050000;
+        public static final int orange4=0x7f050001;
+        public static final int orange5=0x7f050002;
+        public static final int redEnd=0x7f05000c;
+        public static final int redMid=0x7f05000b;
+        public static final int redStart=0x7f05000a;
+        public static final int white_color=0x7f050007;
+        public static final int yellow1=0x7f050005;
+        public static final int yellow2=0x7f050006;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -44,8 +51,8 @@ containing a value of this type.
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int green_progress=0x7f020000;
@@ -68,40 +75,40 @@ containing a value of this type.
         public static final int dataLayout=0x7f0a0011;
         public static final int date=0x7f0a0028;
         public static final int endButton=0x7f0a0010;
-        public static final int historyButton=0x7f0a0000;
-        public static final int historyLayout=0x7f0a000b;
-        public static final int list=0x7f0a0002;
+        public static final int historyButton=0x7f0a0002;
+        public static final int historyLayout=0x7f0a0000;
+        public static final int list=0x7f0a0006;
         public static final int maxProgressBar=0x7f0a0023;
         public static final int max_value=0x7f0a0027;
         public static final int menu_settings=0x7f0a0029;
         public static final int min_value=0x7f0a0026;
         public static final int pauseButton=0x7f0a000f;
-        public static final int percent=0x7f0a0007;
-        public static final int progressBar1=0x7f0a0022;
+        public static final int percent=0x7f0a0009;
+        public static final int progressBar1=0x7f0a0021;
         public static final int progressBarG=0x7f0a0019;
         public static final int progressBarR=0x7f0a001a;
-        public static final int progressBarTitle=0x7f0a0021;
-        public static final int recorded=0x7f0a0009;
+        public static final int progressBarTitle=0x7f0a0020;
+        public static final int recorded=0x7f0a000a;
         public static final int seek_bar=0x7f0a0025;
-        public static final int spoke=0x7f0a0008;
+        public static final int spoke=0x7f0a000b;
         public static final int startButton=0x7f0a000e;
         public static final int startStopPauceLayout=0x7f0a000d;
-        public static final int textView1=0x7f0a0003;
-        public static final int textView2=0x7f0a0005;
-        public static final int textView3=0x7f0a0004;
-        public static final int textView4=0x7f0a0006;
+        public static final int textView1=0x7f0a0004;
+        public static final int textView2=0x7f0a0007;
+        public static final int textView3=0x7f0a0005;
+        public static final int textView4=0x7f0a0008;
         public static final int thresholdBarTitle=0x7f0a001c;
         public static final int thresholdLayout=0x7f0a001b;
         public static final int thresholdSBTitle=0x7f0a0016;
         public static final int thresholdSeekBar=0x7f0a001f;
-        public static final int timeElapsedLayout=0x7f0a0020;
-        public static final int topAnchorLayout=0x7f0a000a;
-        public static final int ttmlogoTopIcon=0x7f0a000c;
+        public static final int timeElapsedLayout=0x7f0a0003;
+        public static final int topAnchorLayout=0x7f0a000c;
         public static final int voiceLevelLayout=0x7f0a0015;
         public static final int voiceLevelSoundOff=0x7f0a0017;
         public static final int voiceLevelSoundOn=0x7f0a0018;
         public static final int voiceThreshSoundOff=0x7f0a001d;
         public static final int voiceThreshSoundOn=0x7f0a001e;
+        public static final int zeroProgressBar=0x7f0a0022;
     }
     public static final class layout {
         public static final int activity_history=0x7f030000;
@@ -116,17 +123,17 @@ containing a value of this type.
         public static final int recording__length=0x7f090002;
     }
     public static final class string {
-        public static final int action_settings=0x7f060009;
-        public static final int app_name=0x7f060000;
-        public static final int dialog_title=0x7f060006;
-        public static final int hello_world=0x7f060001;
-        public static final int menu_settings=0x7f060002;
-        public static final int settings_app_name=0x7f060004;
-        public static final int summary=0x7f060005;
-        public static final int time_dialog_title=0x7f060007;
-        public static final int title_activity_history=0x7f06000a;
-        public static final int title_activity_recording__length=0x7f060008;
-        public static final int title_activity_sound_analyser=0x7f060003;
+        public static final int action_settings=0x7f070009;
+        public static final int app_name=0x7f070000;
+        public static final int dialog_title=0x7f070006;
+        public static final int hello_world=0x7f070001;
+        public static final int menu_settings=0x7f070002;
+        public static final int settings_app_name=0x7f070004;
+        public static final int summary=0x7f070005;
+        public static final int time_dialog_title=0x7f070007;
+        public static final int title_activity_history=0x7f07000a;
+        public static final int title_activity_recording__length=0x7f070008;
+        public static final int title_activity_sound_analyser=0x7f070003;
     }
     public static final class style {
         public static final int AppTheme=0x7f080000;
