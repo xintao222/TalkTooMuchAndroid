da012bb7a444ded60077d1c792f07f16014aaae7
mrickert <mrickert@gwmail.gwu.edu>
2013-05-06 17:44:31 -0400

    final version as of May 6

---
+++ Talk Too Much/gen/com/Namkour/soundanalyser/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.Namkour.soundanalyser;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Talk Too Much/src/com/Namkour/soundanalyser/Recording.java
@@ -0,0 +1,41 @@
+package com.Namkour.soundanalyser;
+
+public class Recording {
+	
+	  String date = null;
+	  long talk_time = 0;
+	  long total_time = 0;
+	  int percent = 0;
+
+	  public String getDate(){
+		  return date;
+	  }
+	  
+	  public void setDate(String date){
+		  this.date = date;
+	  }
+
+	  public long getTalkTime() {
+	    return talk_time;
+	  }
+
+	  public void setTalkTime(long talk_time) {
+	    this.talk_time = talk_time;
+	  }
+
+	  public long getTotalTime() {
+		  return total_time;
+	  }
+	  
+	  public void setTotalTime(long total_time){
+		  this.total_time = total_time;
+	  }
+	  
+	  public int getPercent() {
+		  return percent;
+	  }
+	  public void setPercent(int percent) {
+		  this.percent = percent;
+	  }
+}
+
